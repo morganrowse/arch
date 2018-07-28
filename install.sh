e5ec3b45e2d2b524cf2cf66b736a5896fac0d0cc
Morgan <morgan.rowse@gmail.com>
2018-07-28 15:24:09 +0200

    init

---
+++ install.sh
@@ -0,0 +1,394 @@
+#!/bin/bash
+
+# Drive to install to.
+DRIVE='/dev/sda'
+
+# Hostname of the installed machine.
+HOSTNAME='arch'
+
+# Root password (leave blank to be prompted).
+ROOT_PASSWORD=''
+
+# Main user to create (by default, added to wheel group, and others).
+USER_NAME='morgan'
+
+# The main user's password (leave blank to be prompted).
+USER_PASSWORD=''
+
+# System timezone.
+TIMEZONE='Africa/Johannesburg'
+
+# Have /tmp on a tmpfs or not.  Leave blank to disable.
+# Only leave this blank on systems with very little RAM.
+TMP_ON_TMPFS='TRUE'
+
+KEYMAP='us'
+# KEYMAP='dvorak'
+
+# Choose your video driver
+# For Intel
+#VIDEO_DRIVER="i915"
+# For nVidia
+VIDEO_DRIVER="nouveau"
+# For ATI
+#VIDEO_DRIVER="radeon"
+# For generic stuff
+#VIDEO_DRIVER="vesa"
+
+setup() {
+    local boot_dev="$DRIVE"1
+    local lvm_dev="$DRIVE"2
+
+    echo 'Creating partitions'
+    partition_drive "$DRIVE"
+
+    local lvm_part="$lvm_dev"
+
+    echo 'Setting up LVM'
+    setup_lvm "$lvm_part" vg00
+
+    echo 'Formatting filesystems'
+    format_filesystems "$boot_dev"
+
+    echo 'Mounting filesystems'
+    mount_filesystems "$boot_dev"
+
+    echo 'Installing base system'
+    install_base
+
+    echo 'Chrooting into installed system to continue setup...'
+    cp $0 /mnt/setup.sh
+    arch-chroot /mnt ./setup.sh chroot
+
+    if [ -f /mnt/setup.sh ]
+    then
+        echo 'ERROR: Something failed inside the chroot, not unmounting filesystems so you can investigate.'
+        echo 'Make sure you unmount everything before you try to run this script again.'
+    else
+        echo 'Unmounting filesystems'
+        unmount_filesystems
+        echo 'Done! Reboot system.'
+    fi
+}
+
+configure() {
+    local boot_dev="$DRIVE"1
+    local lvm_dev="$DRIVE"2
+
+    echo 'Installing additional packages'
+    install_packages
+
+    echo 'Installing AUR packages'
+    install_aur_packages
+
+    echo 'Clearing package tarballs'
+    clean_packages
+
+    echo 'Setting hostname'
+    set_hostname "$HOSTNAME"
+
+    echo 'Setting timezone'
+    set_timezone "$TIMEZONE"
+
+    echo 'Setting locale'
+    set_locale
+
+    echo 'Setting console keymap'
+    set_keymap
+
+    echo 'Setting hosts file'
+    set_hosts "$HOSTNAME"
+
+    echo 'Setting fstab'
+    set_fstab "$TMP_ON_TMPFS" "$boot_dev"
+
+    echo 'Setting initial modules to load'
+    set_modules_load
+
+    echo 'Configuring initial ramdisk'
+    set_initcpio
+
+    echo 'Setting initial daemons'
+    set_daemons "$TMP_ON_TMPFS"
+
+    echo 'Configuring sudo'
+    set_sudoers
+
+    echo 'Configuring slim'
+    set_slim
+
+    if [ -z "$ROOT_PASSWORD" ]
+    then
+        echo 'Enter the root password:'
+        stty -echo
+        read ROOT_PASSWORD
+        stty echo
+    fi
+    echo 'Setting root password'
+    set_root_password "$ROOT_PASSWORD"
+
+    if [ -z "$USER_PASSWORD" ]
+    then
+        echo "Enter the password for user $USER_NAME"
+        stty -echo
+        read USER_PASSWORD
+        stty echo
+    fi
+    echo 'Creating initial user'
+    create_user "$USER_NAME" "$USER_PASSWORD"
+
+    echo 'Building locate database'
+    update_locate
+
+    rm /setup.sh
+}
+
+partition_drive() {
+    local dev="$1"; shift
+
+    # 100 MB /boot partition, everything else under LVM
+    parted -s "$dev" \
+        mklabel msdos \
+        mkpart primary ext2 1 100M \
+        mkpart primary ext2 100M 100% \
+        set 1 boot on \
+        set 2 LVM on
+}
+
+setup_lvm() {
+    local partition="$1"; shift
+    local volgroup="$1"; shift
+
+    pvcreate "$partition"
+    vgcreate "$volgroup" "$partition"
+
+    # Create a 1GB swap partition
+    lvcreate -C y -L1G "$volgroup" -n swap
+
+    # Use the rest of the space for root
+    lvcreate -l '+100%FREE' "$volgroup" -n root
+
+    # Enable the new volumes
+    vgchange -ay
+}
+
+format_filesystems() {
+    local boot_dev="$1"; shift
+
+    mkfs.ext2 -L boot "$boot_dev"
+    mkfs.ext4 -L root /dev/vg00/root
+    mkswap /dev/vg00/swap
+}
+
+mount_filesystems() {
+    local boot_dev="$1"; shift
+
+    mount /dev/vg00/root /mnt
+    mkdir /mnt/boot
+    mount "$boot_dev" /mnt/boot
+    swapon /dev/vg00/swap
+}
+
+install_base() {
+    echo 'Server = http://mirrors.kernel.org/archlinux/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
+
+    pacstrap /mnt base base-devel
+}
+
+unmount_filesystems() {
+    umount /mnt/boot
+    umount /mnt
+    swapoff /dev/vg00/swap
+    vgchange -an
+}
+
+install_packages() {
+    local packages=''
+
+    # General utilities/libraries
+    packages+=' alsa-utils sudo'
+
+    # Development packages
+    packages+=' git'
+
+    # Xserver
+    packages+=' xorg-apps xorg-server xorg-xinit xterm'
+
+    # On Intel processors
+    packages+=' intel-ucode'
+
+    if [ "$VIDEO_DRIVER" = "i915" ]
+    then
+        packages+=' xf86-video-intel libva-intel-driver'
+    elif [ "$VIDEO_DRIVER" = "nouveau" ]
+    then
+        packages+=' xf86-video-nouveau'
+    elif [ "$VIDEO_DRIVER" = "radeon" ]
+    then
+        packages+=' xf86-video-ati'
+    elif [ "$VIDEO_DRIVER" = "vesa" ]
+    then
+        packages+=' xf86-video-vesa'
+    fi
+
+    pacman -Sy --noconfirm $packages
+}
+
+clean_packages() {
+    yes | pacman -Scc
+}
+
+set_hostname() {
+    local hostname="$1"; shift
+
+    echo "$hostname" > /etc/hostname
+}
+
+set_timezone() {
+    local timezone="$1"; shift
+
+    ln -sT "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
+}
+
+set_locale() {
+    echo 'LANG="en_US.UTF-8"' >> /etc/locale.conf
+    echo 'LC_COLLATE="C"' >> /etc/locale.conf
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+}
+
+set_keymap() {
+    echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+}
+
+set_hosts() {
+    local hostname="$1"; shift
+
+    cat > /etc/hosts <<EOF
+127.0.0.1 localhost.localdomain localhost $hostname
+::1       localhost.localdomain localhost $hostname
+EOF
+}
+
+set_fstab() {
+    local tmp_on_tmpfs="$1"; shift
+    local boot_dev="$1"; shift
+
+    local boot_uuid=$(get_uuid "$boot_dev")
+
+    cat > /etc/fstab <<EOF
+#
+# /etc/fstab: static file system information
+#
+# <file system> <dir>    <type> <options>    <dump> <pass>
+
+/dev/vg00/swap none swap  sw                0 0
+/dev/vg00/root /    ext4  defaults,relatime 0 1
+
+UUID=$boot_uuid /boot ext2 defaults,relatime 0 2
+EOF
+}
+
+set_modules_load() {
+    echo 'microcode' > /etc/modules-load.d/intel-ucode.conf
+}
+
+set_initcpio() {
+    local vid
+
+    if [ "$VIDEO_DRIVER" = "i915" ]
+    then
+        vid='i915'
+    elif [ "$VIDEO_DRIVER" = "nouveau" ]
+    then
+        vid='nouveau'
+    elif [ "$VIDEO_DRIVER" = "radeon" ]
+    then
+        vid='radeon'
+    fi
+
+    # Set MODULES with your video driver
+    cat > /etc/mkinitcpio.conf <<EOF
+
+# MODULES
+MODULES="ext4 $vid"
+
+HOOKS="base udev autodetect modconf block keymap keyboard $encrypt lvm2 resume filesystems fsck"
+EOF
+
+    mkinitcpio -p linux
+}
+
+set_daemons() {
+    local tmp_on_tmpfs="$1"; shift
+
+    systemctl enable dhcpcd@eth0.service
+
+    if [ -z "$tmp_on_tmpfs" ]
+    then
+        systemctl mask tmp.mount
+    fi
+}
+
+set_sudoers() {
+    cat > /etc/sudoers <<EOF
+##
+## User privilege specification
+##
+root ALL=(ALL) ALL
+
+## Uncomment to allow members of group wheel to execute any command
+%wheel ALL=(ALL) ALL
+
+## Same thing without a password
+# %wheel ALL=(ALL) NOPASSWD: ALL
+
+## Uncomment to allow members of group sudo to execute any command
+# %sudo ALL=(ALL) ALL
+
+## Uncomment to allow any user to run sudo if they know the password
+## of the user they are running the command as (root by default).
+# Defaults targetpw  # Ask for the password of the target user
+# ALL ALL=(ALL) ALL  # WARNING: only use this together with 'Defaults targetpw'
+
+%rfkill ALL=(ALL) NOPASSWD: /usr/sbin/rfkill
+%network ALL=(ALL) NOPASSWD: /usr/bin/netcfg, /usr/bin/wifi-menu
+
+## Read drop-in files from /etc/sudoers.d
+## (the '#' here does not indicate a comment)
+#includedir /etc/sudoers.d
+EOF
+
+    chmod 440 /etc/sudoers
+}
+
+set_root_password() {
+    local password="$1"; shift
+
+    echo -en "$password\n$password" | passwd
+}
+
+create_user() {
+    local name="$1"; shift
+    local password="$1"; shift
+
+    useradd -m -s /bin/zsh -G adm,systemd-journal,wheel,rfkill,games,network,video,audio,optical,floppy,storage,scanner,power,adbusers,wireshark "$name"
+    echo -en "$password\n$password" | passwd "$name"
+}
+
+update_locate() {
+    updatedb
+}
+
+get_uuid() {
+    blkid -o export "$1" | grep UUID | awk -F= '{print $2}'
+}
+
+set -ex
+
+if [ "$1" == "chroot" ]
+then
+    configure
+else
+    setup
+fi
